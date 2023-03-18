c594e5a1085a12f2e73503207e4f96293e2a6de7
Andre Murano <onesartstudio@gmail.com>
2023-03-18 02:38:32 +0200

    Bean scoup (singleton & prototype

--- src/main/java/ua/studio/springcourse/TestSpring.java
+++ src/main/java/ua/studio/springcourse/TestSpring.java
@@ -18,9 +18,19 @@ public class TestSpring {
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        firstMusicPlayer.playMusic();
+        MusicPlayer secondMusicPlayer= context.getBean("musicPlayer", MusicPlayer.class);
+        secondMusicPlayer.playMusic();
 
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+        firstMusicPlayer.setVolume(20);
+        System.out.println(firstMusicPlayer.getVolume()+ "% volume in first music player");
+        System.out.println(secondMusicPlayer.getVolume()+ "% volume in second music player");
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
         context.close();
