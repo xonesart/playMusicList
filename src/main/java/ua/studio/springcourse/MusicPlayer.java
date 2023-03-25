388c0159afdf9306a13a58fc13f9b7ff5ab2f3a8
Andre Murano <onesartstudio@gmail.com>
2023-03-25 22:09:34 +0200

    Annotations @Qualifier

--- src/main/java/ua/studio/springcourse/MusicPlayer.java
+++ src/main/java/ua/studio/springcourse/MusicPlayer.java
@@ -1,22 +1,26 @@
 package ua.studio.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private PopMusic popMusic;
+       private Music music1;
+    private Music music2;
+    private Music music3;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, PopMusic popMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-        this.popMusic = popMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2,
+                       @Qualifier("popMusic") Music music3) {
+        this.music1 = music1;
+        this.music2 = music2;
+        this.music3 = music3;
     }
-    public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+
+       public String playMusic() {
+        return "Playing: " + music1.getSong() + ", " + music2.getSong() + ", " + music3.getSong();
     }
 
 }
