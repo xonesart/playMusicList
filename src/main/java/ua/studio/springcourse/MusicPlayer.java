758c35f7b3804c10a4211bef16e281a9e94326fc
Andre Murano <onesartstudio@gmail.com>
2023-03-25 16:48:05 +0200

    Annotations @Autowired

--- src/main/java/ua/studio/springcourse/MusicPlayer.java
+++ src/main/java/ua/studio/springcourse/MusicPlayer.java
@@ -1,13 +1,22 @@
 package ua.studio.springcourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+    private PopMusic popMusic;
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, PopMusic popMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
+        this.popMusic = popMusic;
+    }
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
 }
--- src/main/java/ua/studio/springcourse/TestSpring.java
+++ src/main/java/ua/studio/springcourse/TestSpring.java
@@ -11,19 +11,8 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-
-           Music music = context.getBean("rockMusic", Music.class);
-           MusicPlayer musicPlayer = new MusicPlayer(music);
-           musicPlayer.playMusic();
-
-           Music music1 = context.getBean("classicalMusic", Music.class);
-           MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-           musicPlayer1.playMusic();
-
-           Music music2 = context.getBean("popMusic", Music.class);
-           MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-           musicPlayer2.playMusic();
-
+           Computer computer = context.getBean("computer", Computer.class);
+           System.out.println(computer);
         context.close();
         // - закриває контекст Spring.
     }
